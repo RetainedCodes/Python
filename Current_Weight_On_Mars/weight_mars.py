c2e5a1a2f2b3b7131995430dcc2d00b3f993d5d4
RetainedCodes <88753748+RetainedCodes@users.noreply.github.com>
2021-09-15 16:24:46 +1000

    add stuff

--- Current_Weight_On_Mars/weight_mars.py
+++ Current_Weight_On_Mars/weight_mars.py
@@ -1,4 +1,4 @@
-current_weight = int(input("How much do you weigh Currently? (kg please) "))
+current_weight = int(input("How much do you weigh Currently? (kg please) Also no need to put kg at the end "))
 mars_weight = current_weight * 0.38
 
 print("You will weigh on mars (it will be in kg)")
