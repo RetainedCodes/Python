c479bf9c7cb92390c54e4f48e5d847c9ff5ba4a6
RetainedCodes <88753748+RetainedCodes@users.noreply.github.com>
2021-09-16 21:35:59 +1000

    added madlibs game

---
+++ Mad Libs/madLibs.py
@@ -0,0 +1,11 @@
+#Mad Libs game by RetainedCodes
+#Enjoy!
+
+colour = input("Enter any Colour")
+food = input("Enter any Food")
+techCompany = input("Enter any TechCompany")
+
+print(f"My favourite Colour is {colour} and I enjoy eating {food} and {techCompany} makes the best stuff!")
+
+print("Thank you for playing this simple madLibs game!")
+
---
+++ Mad Libs/readme.md
@@ -0,0 +1,3 @@
+Hi, this is just a simple Mad Libs game enjoy!
+
+This code is open-source can I don't care if you clone it
