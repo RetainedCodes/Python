0f304493580626ce8bb11c3843a0a983ef4d8fb2
RetainedCodes <88753748+RetainedCodes@users.noreply.github.com>
2021-09-21 21:54:45 +1000

    ended with period

--- EvenOrOdd/evenodd.py
+++ EvenOrOdd/evenodd.py
@@ -3,9 +3,8 @@ number = int(input("Enter Number: "))
 evenodd = number % 2 
 
 if evenodd > 0:
-    print("The Number Entered is Odd ")
+    print("The Number Entered is Odd. ")
 
 else:
     print("The Number Entered is Even.")
 
-    
