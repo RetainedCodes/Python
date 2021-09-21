5a2b2e5b5aa501e88e4fd16f75d38d7627b7155f
RetainedCodes <88753748+RetainedCodes@users.noreply.github.com>
2021-09-21 21:53:42 +1000

    commit

---
+++ EvenOrOdd/evenodd.py
@@ -0,0 +1,11 @@
+number = int(input("Enter Number: "))
+
+evenodd = number % 2 
+
+if evenodd > 0:
+    print("The Number Entered is Odd ")
+
+else:
+    print("The Number Entered is Even.")
+
+    
